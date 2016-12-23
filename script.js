e8825ccc813b572982a398cc7ea7015e3727ce49
baursak <baursak@gmail.com>
2016-12-23 09:51:05 +0300

    day 3 - lesson 1 - 00h10m25s - function with object as parameter - ver. 2

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function showString(param) {
+    if (!param) param = new Object();
+    if (!param.color) param.color = 'black';
+    if (!param.string) param.string = 'Hello';
     document.write(
-        '<span style="color:' + param.color + '">' + param.string + '</span>');
+        '<span style="color:' + param.color + '">' + param.string + '</span><br>');
 }
 showString({color: 'red', string: 'Hello world'});
+showString({color: 'red'});
+showString({string: 'Hello John'});
+showString();
