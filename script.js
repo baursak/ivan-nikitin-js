083fd51ed493f733ab29234e12dd3be60f75204f
baursak <baursak@gmail.com>
2016-12-24 22:57:00 +0300

    day3-lesson2-list of document object props

--- script.js
+++ script.js
@@ -1,23 +1,7 @@
-/*
- * home work
- */
-var dates = [];
 var i;
-for (i = 1; i <= 10; i++) {
-    dates.push(new Date(2016, 11, i));
-}
-console.log(dates);
-var days = [
-    'Воскресенье',
-    'Понедельник',
-    'Вторник',
-    'Среда',
-    'Четверг',
-    'Пятница',
-    'Суббота',
-];
-for (j in dates) {
-    document.write(dates[j].toLocaleDateString()
-        + ' - ' + days[dates[j].getDay()]
-        + '<br>');
+for(i in document){
+    document.write(i);
+    document.write(": ");
+    document.write(document[i]);
+    document.write("<br>");
 }
