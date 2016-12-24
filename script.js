6035709002b809a83e4067ab3187aa63ce738dd4
baursak <baursak@gmail.com>
2016-12-24 23:40:59 +0300

    day4-lesson1-00h11m10s : old way of JS script launch via link in html

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-window.onload = function(){
-    var h2 = document.createElement('h2');
-    h2.appendChild(document.createTextNode('Hello all'));
-    document.body.appendChild(h2);
-};
+function foo(){
+    alert("hello");
+}
+// собственно запуск функции foo - в html файле
