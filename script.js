a3cbea8da6735e3b454c77edc211b8d94ff36514
baursak <baursak@gmail.com>
2016-12-25 12:05:43 +0300

    day4-lesson1-00h59m36s : timer in <h1>

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-function foo(){
-    alert("go to Google");
+window.onload = function () {
+    showTime();
+    setInterval('showTime()', 1000);
+};
+function showTime(){
+    var h1 = document.getElementsByTagName('h1')[0];
+    h1.innerHTML = 'Current time is ' + new Date().toLocaleTimeString();
 }
-// собственно запуск функции foo - в html файле
